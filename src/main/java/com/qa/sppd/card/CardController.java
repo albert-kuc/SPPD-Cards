ed5be1f82942cae396f3a759eaaee4b83d25df8c
Albert Kuc <albert.kuc@gmail.com>
2021-11-10 23:23:55 +0000

    SPPD-4 #in-progress #comment create card service and dependency inject to card controller

--- src/main/java/com/qa/sppd/card/CardController.java
+++ src/main/java/com/qa/sppd/card/CardController.java
@@ -1,49 +1,44 @@
 package com.qa.sppd.card;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "card")
+@RequestMapping(path="card")
 public class CardController {
 
-    private final List<Card> cards = new ArrayList<>();
+    private final CardService cardService;
 
-    @GetMapping(path = "/hello")
-    public String hello() {
-        return "Hello world!";
+    @Autowired
+    public CardController(CardService cardService) {
+        this.cardService = cardService;
     }
 
     @PostMapping(path="create")
     public ResponseEntity<Card> createCard(@RequestBody Card newCard) {
-
-        this.cards.add(newCard);
-        return new ResponseEntity<>(this.cards.get(this.cards.size() - 1), HttpStatus.CREATED);
+        return this.cardService.createCard(newCard);
     }
 
     @GetMapping(path="getAll")
     public ResponseEntity<List<Card>> getAllCards() {
-        return ResponseEntity.ok(this.cards);
+        return this.cardService.getAllCards();
     }
 
     @GetMapping(path="get/{idx}")
     public ResponseEntity<Card> getCardByIndex(@PathVariable Integer idx) {
-        return ResponseEntity.ok(this.cards.get(idx));
+        return this.cardService.getCardByIndex(idx);
     }
 
     @PutMapping(path="replace/{idx}")
     public ResponseEntity<Card> replaceCard(@PathVariable Integer idx, @RequestBody Card newCard) {
-        this.cards.set(idx, newCard);
-        return new ResponseEntity<>(this.cards.get(idx), HttpStatus.ACCEPTED);
+        return this.cardService.replaceCard(idx, newCard);
     }
 
     @DeleteMapping(path="remove/{idx}")
     public ResponseEntity<?> removeCard(@PathVariable int idx) {
-        this.cards.remove(idx);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return this.cardService.removeCard(idx);
     }
 }
