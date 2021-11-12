ced2b911a36c4fa6f5071f58e37d92bc5a4174cc
Albert Kuc <albert.kuc@gmail.com>
2021-11-12 15:03:10 +0000

    Revert "Merge branch 'dev' to documentation"
    This reverts commit b9f1271c77b264cd495608bb26d569bd6aa6a19e, reversing
    changes made to da9b6c5549997f99a7fb75a9fbb4a50819fc2466.

--- src/test/java/com/qa/sppd/card/CardIntegrationTest.java
+++ src/test/java/com/qa/sppd/card/CardIntegrationTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -25,7 +24,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @AutoConfigureMockMvc
 @Sql(scripts={"classpath:card-schema.sql", "classpath:card-data.sql"},
         executionPhase=Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@ActiveProfiles("test")
 public class CardIntegrationTest {
 
     @Autowired
