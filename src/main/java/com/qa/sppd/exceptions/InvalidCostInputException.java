ee336d4ac0bb9fada30e75be8e3a003a2eb9d9a3
Albert Kuc <albert.kuc@gmail.com>
2021-11-14 23:16:49 +0000

    SPPD-20 #comment update exception messages

--- src/main/java/com/qa/sppd/exceptions/InvalidCostInputException.java
+++ src/main/java/com/qa/sppd/exceptions/InvalidCostInputException.java
@@ -3,7 +3,7 @@ package com.qa.sppd.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE, reason = "Cost value must be integer in range (1, 7) inclusive")
+@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE, reason = "Cost value must be an integer in range (1, 7) inclusive")
 public class InvalidCostInputException extends IllegalArgumentException{
 
     public InvalidCostInputException(String s) {
